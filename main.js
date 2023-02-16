dacfed41702f5fae036d9eea64f5ba0e7a36792e
Sciencewolf <aron17marton@gmail.com>
2023-02-16 20:38:43 +0100

    IDK

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
-let log = console.log
-
 function onLoad() {
-    const year = document.getElementById('year-footer')
-    let _date = new Date()
-    let _year = _date.getUTCFullYear()
-    year.innerHTML = _year
+  const year = document.getElementById("year-footer");
+  let _date = new Date();
+  let _year = _date.getUTCFullYear();
+  year.innerHTML = _year;
+}
+
+function sendData() {
+  const input_field = document.getElementById("input_field");
+  const btn_send = document.getElementById("btn-send");
+
+  let val = input_field.value;
+
+  btn_send.addEventListener("click", () => {
+    
+  });
 }
